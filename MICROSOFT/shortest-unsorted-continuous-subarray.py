f1563103c94a950132c3d343a0adc8f883c1cb79
utkarshprap <utkarshprap@gmail.com>
2023-01-04 02:26:41 +0530

    added soln

---
+++ MICROSOFT/shortest-unsorted-continuous-subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findUnsortedSubarray(self, nums):
+        n=len(nums)
+        if n<2: return 0
+        prev,end = nums[0],0
+        for i in range(n):
+            if nums[i]<prev: end=i
+            else: prev=nums[i]
+        start,prev=n-1,nums[n-1]
+        for i in range(n-1,-1,-1):
+            if prev<nums[i]: start=i
+            else: prev=nums[i]
+        if end!=0: return end-start+1
+        else: return 0
