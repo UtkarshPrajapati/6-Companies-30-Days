1067b8aaa1d88a4b981d0361069296090fe1d240
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-01-09 19:34:35 +0530

    added soln

--- Goldman Sachs/count-good-triplets-in-an-array.py
+++ Goldman Sachs/count-good-triplets-in-an-array.py
@@ -1,20 +1,11 @@
-from sortedcontainers import SortedList as sl
+from sortedcontainers import SortedList
 class Solution:
-    def goodTriplets(self, A: List[int], B: List[int]) -> int:
-        p=[0]*len(A)               
-        for i,b in enumerate(B):
-            p[b]=i
-        pb,prea=sl([p[A[0]]]),[0]      
-        for a in A[1:]:       
-            pb.add(p[a])
-            prea.append(pb.bisect_left(p[a]))
-        pb,sufa=sl([p[A[-1]]]),[0]
-        for a in reversed(A[:len(A)-1]):
-            i=pb.bisect(p[a])
-            sufa.append(len(pb)-i)
-            pb.add(p[a])
-        sufa.reverse()
-        ans=0
-        for x,y in zip(prea,sufa):
-            ans+=x*y
+    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        sl,ans=SortedList(),0
+        mp={x:i for i,x in enumerate(nums1)}
+        for i,x in enumerate(nums2):
+            x=mp[x]
+            left=sl.bisect_left(x)
+            ans+=(left*((len(nums2)-x-1)-(len(sl)-left)))
+            sl.add(x)
         return ans
