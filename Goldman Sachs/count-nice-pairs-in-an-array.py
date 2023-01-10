170cdbc7d785df379d8003e31cd66943578db8ba
Utkarsh Prajapati <utkarshprap@gmail.com>
2023-01-11 02:15:39 +0530

    updated soln

--- Goldman Sachs/count-nice-pairs-in-an-array.py
+++ Goldman Sachs/count-nice-pairs-in-an-array.py
@@ -3,9 +3,9 @@ class Solution:
         s,d=0,{}
         for i,a in enumerate(nums):
             k=a-int(str(a)[::-1])
-            if k in d.keys(): d[k]+=[i]
-            else: d[k]=[i]
+            if k in d.keys(): d[k]+=1
+            else: d[k]=1
         for i in d.keys():
-            m=len(d[i])
-            s+=(m*(m-1)//2)%(1000000007)
+            m=d[i]
+            s+=m*(m-1)//2%(1000000007)
         return s%(1000000007)
